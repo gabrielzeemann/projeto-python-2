4f569f4fbcf3c9a21d6ae41eb6db4e33a312f906
Gabriel <japrendiz_gabrielcs@ciasc.sc.gov.br>
2023-10-16 15:25:49 -0300

    primeiro comit

---
+++ aula01/app.py
@@ -0,0 +1,40 @@
+num1 = 5
+num2 = 3.5
+
+a = float(num1)
+b = int(num2)
+
+a = int(float("5.3"))
+b = "3.5"
+
+num1 + num2
+num1 - num2
+num1 * num2
+num1 /  num2
+num1 % num2
+num1 ** num2
+num1 // num2 
+
+3 + 5 * 7 + 3
+(3 + 5) * (7 + 3)
+
+print(abs(-15))
+print(pow(3,3))
+print(max(1,85,17,-55,5))
+print(max(1,85,17,-55,5))
+print(roud(8.8))
+
+
+
+print(math. floor(8.8))
+
+
+
+
+
+
+
+
+
+
+
---
+++ aula01/teste.py
@@ -0,0 +1,9 @@
+n = "2"
+n = float(n)
+print(type(n))
+print(n)
+
+bool #valor boleano
+str #string
+int #inteiro
+float #numero 'quebrado' com virgula
